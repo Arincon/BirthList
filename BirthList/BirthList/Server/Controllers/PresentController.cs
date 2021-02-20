41be5442e23b85f3d91163ed4a6547236f74f77e
Adrián Del Rincón López <arincon@encamina.com>
2021-02-20 17:43:30 +0100

    Changes app

---
+++ BirthList/BirthList/Server/Controllers/PresentController.cs
@@ -0,0 +1,35 @@
+﻿using BirthList.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirthList.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PresentController : ControllerBase
+    {
+        private readonly ILogger<PresentController> _logger;
+
+        public PresentController(ILogger<PresentController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Present> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 10).Select(index => new Present
+            {
+                Id = index,
+                Title = $"Elemento {index}",
+                Description = $"Descripción elemento {index}"
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ BirthList/BirthList/Shared/Present.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BirthList.Shared
+{
+    public class Present
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int RequiredAmount { get; set; }
+        public int RemainingAmount { get; set; }
+        public Uri ImageLink { get; set; }
+        public Uri SampleLink { get; set; }
+    }
+}
